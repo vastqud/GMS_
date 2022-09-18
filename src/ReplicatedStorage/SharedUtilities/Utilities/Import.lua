36db87ec14442f920725d3f82822e0a313f50219
vastqud <66101476+vastqud@users.noreply.github.com>
2022-09-18 01:46:53 -0400

    Update Import.lua

--- src/ReplicatedStorage/SharedUtilities/Utilities/Import.lua
+++ src/ReplicatedStorage/SharedUtilities/Utilities/Import.lua
@@ -1,13 +1,67 @@
 --!strict
 local Import = {};
 
+local ValidRoots = {
+    ["ServerScriptService"] = {
+        Aliases = {
+            "server"
+        },
+        Location = game:GetService("ServerScriptService")
+    },
+    ["ReplicatedStorage"] = {
+        Aliases = {
+            "shared",
+            "client"
+        },
+        Location = game:GetService("ReplicatedStorage")
+    }
+};
+
 local PathUtility;
 local import;
+
+--[[
+        Returns a function specific to the calling script that replaces require.
+        The function will search the calling script for the dependency if no root is given ("/dependency") as path argument.
+        If not searching for direct dependencies, it will search with ReplicatedStorage as the default root ("SharedUtilities/Utilities/Paths").
+        A different root can be specified by prefixing it to the path argument ("ServerScriptService/path/to/file").
+        Once the target file is found, the function will require and return that result.
+]]
+
+local function getRoot(root: string): Instance?
+    local lower_root: string = string.lower(root);
+
+    if ValidRoots[root] then
+        return ValidRoots[root].Location;
+    end;
+
+    for rootName, data in pairs(ValidRoots) do
+        local lower_name: string = string.lower(rootName);
+
+        if lower_root == lower_name then
+            return data.Location;
+        end;
+
+        for _, alias in ipairs(data.Aliases) do
+            if lower_root == alias then
+                return data.Location;
+            end
+        end;
+    end;
+
+    return;
+end
 
 function Import.registerFile(file: Instance): any
     return function(path: string): any
         local paths = string.split(path, "/");
-        local root = if paths[1] == "" then file else nil;
+        local root;
+
+        if paths[1] == "" then
+            root = file;
+        else
+            root = getRoot(paths[1])
+        end;
         
         local accessedFile: Instance? = PathUtility:FindInstanceFromPath(path, root);
 
