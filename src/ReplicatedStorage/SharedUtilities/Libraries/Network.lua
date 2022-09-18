02b5537f44f5c3baf27ab6d80394d3d2ccfead62
vastqud <66101476+vastqud@users.noreply.github.com>
2022-09-18 01:27:31 -0400

    Update Network.lua

--- src/ReplicatedStorage/SharedUtilities/Libraries/Network.lua
+++ src/ReplicatedStorage/SharedUtilities/Libraries/Network.lua
@@ -11,26 +11,20 @@ local PathUtility = import("/Paths");
 
 -- Declarations
 local IsServer: boolean = game:GetService("RunService"):IsServer();
-
-type NetworkUtil = {
-    getRemoteEvent: (name: string, path: string) -> RemoteEvent,
-    getRemoteFunction: (name: string, path: string) -> RemoteFunction
-};
-
-local Network: NetworkUtil = {} :: any;
+local Network = {};
 
 local function _findInstanceFromPath(name: string, path: string, className: string): any
     path = path .. "/" .. name;
 
     local root = if className == "RemoteEvent" then RemoteEvents else RemoteFunctions;
-    local found: any? = PathUtility:FindInstanceFromPath(path, root);
+    local found: Instance? = PathUtility:FindInstanceFromPath(path, root);
 
     if (found) and (found:IsA("RemoteEvent") or found:IsA("RemoteFunction")) then  --  If the network instance was found at the path, return it
         return found;
     end;
     --  Network instance not found
     if IsServer then  --  Server will create the instance and parent it to the correct path
-        local new: any = Instance.new(className, found);
+        local new = Instance.new(className, found);
         new.Name = name;
 
         return new;
@@ -47,4 +41,4 @@ function Network.getRemoteFunction(name: string, path: string): RemoteFunction
     return _findInstanceFromPath(name, path, "RemoteFunction");
 end;
 
-return Network :: NetworkUtil;
+return Network;
