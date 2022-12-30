be79f20a95eaa04e06db18954f7b6c026d4ac566
vastqud <66101476+vastqud@users.noreply.github.com>
2022-12-30 16:30:56 -0500

    Update Constants.lua

--- src/ReplicatedStorage/SharedData/GlobalConstants/Constants.lua
+++ src/ReplicatedStorage/SharedData/GlobalConstants/Constants.lua
@@ -4,7 +4,7 @@ local Constants = {
     DefaultMaxStamina = 100,
     StaminaDrain = 5,
     StaminaGain = 6.5,
-    StaminaGainDelay = 3,
+    StaminaGainDelay = 4.5,
     DefaultWalkspeed = 13,
     SprintWalkspeed = 19.75
 }
